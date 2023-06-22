2244b48506501a1a9171b03ea488e6ada3d6ce60
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-06-22 21:34:12 +0300

    Project Created

---
+++ src/main/java/com/udemymasterspring/masterudemycourse/MasterUdemyCourseApplication.java
@@ -0,0 +1,13 @@
+package com.udemymasterspring.masterudemycourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterUdemyCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterUdemyCourseApplication.class, args);
+	}
+
+}
