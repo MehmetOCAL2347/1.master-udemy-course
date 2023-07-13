817cf55019b704e58493495b276fedde8c06c07b
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-07-14 01:45:33 +0300

    Different Profiles was created

--- src/main/java/com/udemymasterspring/masterudemycourse/RestController/BasicRestController.java
+++ src/main/java/com/udemymasterspring/masterudemycourse/RestController/BasicRestController.java
@@ -1,5 +1,6 @@
 package com.udemymasterspring.masterudemycourse.RestController;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,23 @@ import java.util.List;
 @RestController
 public class BasicRestController {
 
+    @Autowired
+    private CurrencyServiceConfiguration currencyServiceConfiguration;
+
     @RequestMapping("/courses")
     public List<Course> retrieveAllCourses(){
         return Arrays.asList(
                 new Course(1, "Java Basic", "mehmetocal"),
-                new Course(2, "Java Master", "mehmetocal")
+                new Course(2, "Java Master", "mehmetocal"),
+                new Course(3, "Java Selenium", "mehmetocal"),
+                new Course(4, "Java Swing", "mehmetocal"),
+                new Course(5, "Autodesk 3Ds Max", "mehmetocal")
         );
+    }
+
+    @RequestMapping("/currency-service")
+    public CurrencyServiceConfiguration getProperties(){
+        return currencyServiceConfiguration;
     }
 
 }
